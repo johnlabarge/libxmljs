f4e73569cf647fd93f8075da09d307d22d54523e
johnlabarge <johnlabarge@gmail.com>
2013-07-19 12:02:10 -0400

    bug fixes

--- src/xml_namespace.cc
+++ src/xml_namespace.cc
@@ -77,10 +77,9 @@ XmlNamespace::XmlNamespace(xmlNs* node) : xml_obj(node)
 
 XmlNamespace::~XmlNamespace()
 {
-    xml_obj->_private = NULL;
-
-    if (xml_obj->context)
+ if (xml_obj && xml_obj->context)
     {
+        xml_obj->_private = NULL;
         // release the hold and allow the document to be freed
         XmlDocument* doc = static_cast<XmlDocument*>(xml_obj->context->_private);
         doc->unref();
